e28ebc0dc7ef96ccf94b081ed0a784b91ec3e98d
Sinda_Liu <dannizi1992@163.com>
2017-05-16 22:33:06 +0800

    项目初始化

---
+++ DYTV/DYTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DYTV
+//
+//  Created by Sinda_Liu on 2017/5/16.
+//  Copyright © 2017年 Sinda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
